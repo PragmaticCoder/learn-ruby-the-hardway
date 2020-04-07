f4c98ab544331cba49ee231a952e0b3a7239cca4
Alvi Akbar <alvi.akbar@realpythonengineer.com>
2020-04-07 04:46:08 -0600

    varibale assignment

---
+++ exercises/exercise_6/app.rb
@@ -0,0 +1,25 @@
+# set types of people t o10
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+
+# more string substitutions and var assignment
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+# output x and y
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
